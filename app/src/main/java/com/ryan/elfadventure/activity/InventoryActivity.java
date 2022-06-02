c2dadb86e479c82f4f26b50e8eb53b9338e53d25
Sergey Zhur <sergey.zhur96@gmail.com>
2022-06-02 14:22:59 +0300

    DT-19: Design inventory layout

--- app/src/main/java/com/ryan/elfadventure/activity/InventoryActivity.java
+++ app/src/main/java/com/ryan/elfadventure/activity/InventoryActivity.java
@@ -2,12 +2,17 @@ package com.ryan.elfadventure.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import com.ryan.elfadventure.R;
 import com.ryan.elfadventure.global.Cell;
@@ -33,20 +38,67 @@ public class InventoryActivity extends AppCompatActivity {
 
         this.findViewById(android.R.id.content).setOnTouchListener(ostListener);
 
+        FrameLayout backpackBtn = findViewById(R.id.backpackBtn);
+        FrameLayout charBtn = findViewById(R.id.charBtn);
+
+        setLayout(R.layout.full_inventory);
+        backpackBtn.setAlpha(0.75F);
+
+        backpackBtn.setOnClickListener(view -> {
+            backpackBtn.setAlpha(0.75F);
+            charBtn.setAlpha(1F);
+            setLayout(R.layout.full_inventory);
+        });
+
+        charBtn.setOnClickListener(view -> {
+            charBtn.setAlpha(0.75F);
+            backpackBtn.setAlpha(1F);
+            setLayout(R.layout.char_inventory);
+        });
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+    }
 
-        final int MAX_COLS = 6;
-        final int INV_SIZE = 12;
+    private void setLayout(int _id) {
+        LinearLayout invLayout = findViewById(R.id.invLayout);
+        LinearLayout inventory = (LinearLayout) getLayoutInflater().inflate(
+                _id, null, false
+        );
+        inventory.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+        if (_id == R.layout.char_inventory)
+            initTable(inventory.findViewById(R.id.invTable), 2, 6);
+        else
+            initTable(inventory.findViewById(R.id.invTable), 6, 6);
 
-        TableLayout invTable = findViewById(R.id.invTable);
+        invLayout.removeAllViews();
+        invLayout.addView(inventory);
+    }
+
+    private void initTable(LinearLayout _layout, int _rowCount, int _columnCount)
+    {
+        final int invSize = _rowCount * _columnCount;
+
         TableRow tableRow = null;
         int count = 0;
-        for (int i = 0; i < INV_SIZE; ++i) {
-            if (count == 0)
-                tableRow = new TableRow(this);
 
-            ImageView imgView = new ImageView(this);
+        // TODO: Remake into double loop
+        for (int i = 0; i < invSize; ++i) {
+            if (count == 0) {
+                tableRow = new TableRow(this);
+                tableRow.setLayoutParams(
+                        new TableLayout.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                0,
+                                1
+                        )
+                );
+            }
+
+            AppCompatImageView imgView = new AppCompatImageView(this);
 
             InventoryState state = Globals.getInstance().getInvState();
             InvertoryManager manager = XmlManager.getInstance().getInventoryManager();
@@ -62,10 +114,19 @@ public class InventoryActivity extends AppCompatActivity {
                     imgView.setImageResource(drawableResourceId);
                 }
             }
+            else
+                imgView.setImageResource(R.drawable.border);
 
             tableRow.addView(imgView);
-            if (++count == MAX_COLS) {
-                invTable.addView(tableRow);
+            imgView.setLayoutParams(
+                    new TableRow.LayoutParams(
+                            0,
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            1
+                    )
+            );
+            if (++count == _columnCount) {
+                _layout.addView(tableRow);
                 count = 0;
             }
         }
--- app/src/main/java/com/ryan/elfadventure/activity/StartActivity.java
+++ app/src/main/java/com/ryan/elfadventure/activity/StartActivity.java
@@ -1,6 +1,9 @@
 package com.ryan.elfadventure.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,13 @@ public class StartActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        Button newBtn = findViewById(R.id.newBtn);
+        newBtn.setOnClickListener(view -> goToMain());
+    }
+
+    private void goToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
